fccce626ae69fccf63a8dadd3040a0b9af12a6a9
colbygatte <colbygatte@gmail.com>
2016-10-06 13:02:33 -0500

    Initial Commit

---
+++ StepperViewControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StepperViewControl
+//
+//  Created by Colby Gatte on 10/6/16.
+//  Copyright © 2016 Colby Gatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StepperViewControlTests/StepperViewControlTests.m
@@ -0,0 +1,39 @@
+//
+//  StepperViewControlTests.m
+//  StepperViewControlTests
+//
+//  Created by Colby Gatte on 10/6/16.
+//  Copyright © 2016 Colby Gatte. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StepperViewControlTests : XCTestCase
+
+@end
+
+@implementation StepperViewControlTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
